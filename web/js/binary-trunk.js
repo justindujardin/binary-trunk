7acb4fd34f0e154d6bbb0c6f0f9c525cf9e67b9b
Justin DuJardin <justin@dujardinconsulting.com>
2012-09-22 13:10:00 -0700

    Add docs and test libs

---
+++ web/js/binary-trunk.js
@@ -0,0 +1,430 @@
+// binary-trunk.coffee v0.0.1
+// Copyright (c) 2012 Justin DuJardin
+// binary-trunk is freely distributable under the MIT license.
+(function() {
+  var DJC, root,
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  root = this;
+
+  DJC = root.DJC = root.DJC || {};
+
+  DJC.BinaryTreeNode = (function() {
+
+    function BinaryTreeNode(left, right) {
+      this.setSide = __bind(this.setSide, this);
+
+      this.visitPostorder = __bind(this.visitPostorder, this);
+
+      this.visitInorder = __bind(this.visitInorder, this);
+
+      this.visitPreorder = __bind(this.visitPreorder, this);
+
+      var _ref;
+      if (left) {
+        this.setLeft(left);
+      }
+      if (right) {
+        this.setRight(right);
+      }
+      if ((_ref = this.parent) == null) {
+        this.parent = void 0;
+      }
+    }
+
+    BinaryTreeNode.prototype.clone = function() {
+      var result;
+      result = new this.constructor();
+      if (this.left) {
+        result.setLeft(this.left.clone());
+      }
+      if (this.right) {
+        result.setRight(this.right.clone());
+      }
+      return result;
+    };
+
+    BinaryTreeNode.prototype.isLeaf = function() {
+      return !this.left && !this.right;
+    };
+
+    BinaryTreeNode.prototype.toString = function() {
+      return "" + this.left + " " + this.right;
+    };
+
+    BinaryTreeNode.prototype.rotate = function() {
+      var grandParent, node, parent;
+      node = this;
+      parent = this.parent;
+      if (!node || !parent) {
+        return;
+      }
+      grandParent = this.parent.parent;
+      if (node === parent.left) {
+        parent.setLeft(node.right);
+        node.right = parent;
+        parent.parent = node;
+      } else {
+        parent.setRight(node.left);
+        node.left = parent;
+        parent.parent = node;
+      }
+      node.parent = grandParent;
+      if (!grandParent) {
+        return;
+      }
+      if (parent === grandParent.left) {
+        return grandParent.left = node;
+      } else {
+        return grandParent.right = node;
+      }
+    };
+
+    BinaryTreeNode.prototype.visitPreorder = function(visitFunction, depth, data) {
+      if (depth == null) {
+        depth = 0;
+      }
+      if (visitFunction) {
+        visitFunction(this, depth, data);
+      }
+      if (this.left) {
+        this.left.visitPreorder(visitFunction, depth + 1, data);
+      }
+      if (this.right) {
+        return this.right.visitPreorder(visitFunction, depth + 1, data);
+      }
+    };
+
+    BinaryTreeNode.prototype.visitInorder = function(visitFunction, depth, data) {
+      if (depth == null) {
+        depth = 0;
+      }
+      if (this.left) {
+        this.left.visitInorder(visitFunction, depth + 1, data);
+      }
+      if (visitFunction) {
+        visitFunction(this, depth, data);
+      }
+      if (this.right) {
+        return this.right.visitInorder(visitFunction, depth + 1, data);
+      }
+    };
+
+    BinaryTreeNode.prototype.visitPostorder = function(visitFunction, depth, data) {
+      if (depth == null) {
+        depth = 0;
+      }
+      if (this.left) {
+        this.left.visitPostorder(visitFunction, depth + 1, data);
+      }
+      if (this.right) {
+        this.right.visitPostorder(visitFunction, depth + 1, data);
+      }
+      if (visitFunction) {
+        return visitFunction(this, depth, data);
+      }
+    };
+
+    BinaryTreeNode.prototype.getRoot = function() {
+      var result;
+      result = this;
+      while (result.parent) {
+        result = result.parent;
+      }
+      return result;
+    };
+
+    BinaryTreeNode.prototype.setLeft = function(child) {
+      var _ref;
+      this.left = child;
+      return (_ref = this.left) != null ? _ref.parent = this : void 0;
+    };
+
+    BinaryTreeNode.prototype.setRight = function(child) {
+      var _ref;
+      this.right = child;
+      return (_ref = this.right) != null ? _ref.parent = this : void 0;
+    };
+
+    BinaryTreeNode.prototype.getSide = function(child) {
+      if (child === this.left) {
+        return 'left';
+      }
+      if (child === this.right) {
+        return 'right';
+      }
+      throw new Error("BinaryTreeNode.getSide: not a child of this node");
+    };
+
+    BinaryTreeNode.prototype.setSide = function(child, side) {
+      switch (side) {
+        case 'left':
+          return this.setLeft(child);
+        case 'right':
+          return this.setRight(child);
+        default:
+          throw new Error("BinaryTreeNode.setSide: Invalid side");
+      }
+    };
+
+    BinaryTreeNode.prototype.getChildren = function() {
+      var result;
+      result = [];
+      if (this.left) {
+        result.push(this.left);
+      }
+      if (this.right) {
+        result.push(this.right);
+      }
+      return result;
+    };
+
+    BinaryTreeNode.prototype.getSibling = function() {
+      if (!this.parent) {
+        return;
+      }
+      if (this.parent.left === this) {
+        return this.parent.right;
+      }
+      if (this.parent.right === this) {
+        return this.parent.left;
+      }
+    };
+
+    return BinaryTreeNode;
+
+  })();
+
+  DJC.BinarySearchTree = (function(_super) {
+
+    __extends(BinarySearchTree, _super);
+
+    function BinarySearchTree(key) {
+      this.key = key;
+      BinarySearchTree.__super__.constructor.call(this);
+    }
+
+    BinarySearchTree.prototype.clone = function() {
+      var result;
+      result = BinarySearchTree.__super__.clone.call(this);
+      result.key = this.key;
+      return result;
+    };
+
+    BinarySearchTree.prototype.insert = function(key) {
+      var node;
+      node = this.getRoot();
+      while (node) {
+        if (key > node.key) {
+          if (!node.right) {
+            node.setRight(new BinarySearchTree(key));
+            break;
+          }
+          node = node.right;
+        } else if (key < node.key) {
+          if (!node.left) {
+            node.setLeft(new BinarySearchTree(key));
+            break;
+          }
+          node = node.left;
+        } else {
+          break;
+        }
+      }
+      return this;
+    };
+
+    BinarySearchTree.prototype.find = function(key) {
+      var node;
+      node = this.getRoot();
+      while (node) {
+        if (key > node.key) {
+          if (!node.right) {
+            return null;
+          }
+          node = node.right;
+          continue;
+        }
+        if (key < node.key) {
+          if (!node.left) {
+            return null;
+          }
+          node = node.left;
+          continue;
+        }
+        if (key === node.key) {
+          return node;
+        }
+        return null;
+      }
+      return null;
+    };
+
+    return BinarySearchTree;
+
+  })(DJC.BinaryTreeNode);
+
+  DJC.BinaryTreeTidier = (function() {
+
+    function BinaryTreeTidier() {}
+
+    BinaryTreeTidier.prototype.layout = function(node, unitMultiplier) {
+      if (unitMultiplier == null) {
+        unitMultiplier = 1;
+      }
+      this.measure(node);
+      return this.transform(node, 0, unitMultiplier);
+    };
+
+    BinaryTreeTidier.prototype.measure = function(node, level, extremes) {
+      var currentSeparation, left, leftExtremes, leftOffsetSum, minimumSeparation, right, rightExtremes, rightOffsetSum, rootSeparation, _ref, _ref1;
+      if (level == null) {
+        level = 0;
+      }
+      if (extremes == null) {
+        extremes = {
+          left: null,
+          right: null,
+          level: 0
+        };
+      }
+      leftExtremes = {
+        left: null,
+        right: null,
+        level: 0
+      };
+      rightExtremes = {
+        left: null,
+        right: null,
+        level: 0
+      };
+      currentSeparation = 0;
+      rootSeparation = 0;
+      minimumSeparation = 1;
+      leftOffsetSum = 0;
+      rightOffsetSum = 0;
+      if (!node) {
+        if ((_ref = extremes.left) != null) {
+          _ref.level = -1;
+        }
+        if ((_ref1 = extremes.right) != null) {
+          _ref1.level = -1;
+        }
+        return;
+      }
+      node.y = level;
+      left = node.left;
+      right = node.right;
+      this.measure(left, level + 1, leftExtremes);
+      this.measure(right, level + 1, rightExtremes);
+      if (!node.right && !node.left) {
+        node.offset = 0;
+        extremes.right = extremes.left = node;
+        return extremes;
+      }
+      if (!node.right || !node.left) {
+        node.offset = minimumSeparation;
+        extremes.right = extremes.left = node.left ? node.left : node.right;
+        return;
+      }
+      currentSeparation = minimumSeparation;
+      leftOffsetSum = rightOffsetSum = 0;
+      while (left && right) {
+        if (currentSeparation < minimumSeparation) {
+          rootSeparation += minimumSeparation - currentSeparation;
+          currentSeparation = minimumSeparation;
+        }
+        if (left.right) {
+          leftOffsetSum += left.offset;
+          currentSeparation -= left.offset;
+          left = left.thread || left.right;
+        } else {
+          leftOffsetSum -= left.offset;
+          currentSeparation += left.offset;
+          left = left.thread || left.left;
+        }
+        if (right.left) {
+          rightOffsetSum -= right.offset;
+          currentSeparation -= right.offset;
+          right = right.thread || right.left;
+        } else {
+          rightOffsetSum += right.offset;
+          currentSeparation += right.offset;
+          right = right.thread || right.right;
+        }
+      }
+      node.offset = (rootSeparation + 1) / 2;
+      leftOffsetSum -= node.offset;
+      rightOffsetSum += node.offset;
+      if (rightExtremes.left.level > leftExtremes.left.level || !node.left) {
+        extremes.left = rightExtremes.left;
+        extremes.left.offset += node.offset;
+      } else {
+        extremes.left = leftExtremes.left;
+        extremes.left.offset -= node.offset;
+      }
+      if (leftExtremes.right.level > rightExtremes.right.level || !node.right) {
+        extremes.right = leftExtremes.right;
+        extremes.right.offset -= node.offset;
+      } else {
+        extremes.right = rightExtremes.right;
+        extremes.right.offset += node.offset;
+      }
+      if (left && left !== node.left) {
+        rightExtremes.right.thread = left;
+        rightExtremes.right.offset = Math.abs((rightExtremes.right.offset + node.offset) - leftOffsetSum);
+      } else if (right && right !== node.right) {
+        leftExtremes.left.thread = right;
+        leftExtremes.left.offset = Math.abs((leftExtremes.left.offset - node.offset) - rightOffsetSum);
+      }
+      return this;
+    };
+
+    BinaryTreeTidier.prototype.transform = function(node, x, unitMultiplier, measure) {
+      if (x == null) {
+        x = 0;
+      }
+      if (unitMultiplier == null) {
+        unitMultiplier = 1;
+      }
+      if (!measure) {
+        measure = {
+          minX: 10000,
+          maxX: 0,
+          minY: 10000,
+          maxY: 0
+        };
+      }
+      if (!node) {
+        return measure;
+      }
+      node.x = x * unitMultiplier;
+      node.y *= unitMultiplier;
+      this.transform(node.left, x - node.offset, unitMultiplier, measure);
+      this.transform(node.right, x + node.offset, unitMultiplier, measure);
+      if (measure.minY > node.y) {
+        measure.minY = node.y;
+      }
+      if (measure.maxY < node.y) {
+        measure.maxY = node.y;
+      }
+      if (measure.minX > node.x) {
+        measure.minX = node.x;
+      }
+      if (measure.maxX < node.x) {
+        measure.maxX = node.x;
+      }
+      measure.width = Math.abs(measure.minX - measure.maxX);
+      measure.height = Math.abs(measure.minY - measure.maxY);
+      measure.centerX = measure.minX + measure.width / 2;
+      measure.centerY = measure.minY + measure.height / 2;
+      return measure;
+    };
+
+    return BinaryTreeTidier;
+
+  })();
+
+}).call(this);
---
+++ web/js/binary-trunk.test.js
@@ -0,0 +1,390 @@
+// binary-trunk.coffee v0.0.1
+// Copyright (c) 2012 Justin DuJardin
+// binary-trunk is freely distributable under the MIT license.
+(function() {
+
+  module('BinaryTreeNode');
+
+  test('constructor', function() {
+    var count, tree;
+    tree = new DJC.BinaryTreeNode(new DJC.BinaryTreeNode, new DJC.BinaryTreeNode);
+    equal(tree.left !== null && tree.right !== null, true, 'children assigned properly by constructor');
+    count = 0;
+    tree.visitInorder(function(node) {
+      return count++;
+    });
+    equal(count, 3, 'expected tree node count');
+    equal(tree.left.parent === tree, true, 'left child parent assigned properly');
+    return equal(tree.right.parent === tree, true, 'right child parent assigned properly');
+  });
+
+  test('clone', function() {
+    var clone, count, fifteen, i, tree, _i;
+    tree = new DJC.BinarySearchTree(0);
+    for (i = _i = 0; _i <= 25; i = ++_i) {
+      tree.insert(i);
+    }
+    fifteen = tree.find(15);
+    equal(fifteen !== null, true, 'find isolation node');
+    clone = fifteen.clone();
+    equal(clone.getRoot() === clone, true, 'clone node becomes root');
+    count = 0;
+    clone.visitInorder(function() {
+      return count++;
+    });
+    return equal(count, 11, 'clone node has expected 11 remaining nodes');
+  });
+
+  test('isLeaf', function() {
+    var i, n, tree, _i, _j, _results;
+    tree = new DJC.BinarySearchTree(0);
+    for (i = _i = -1; _i <= 5; i = ++_i) {
+      tree.insert(i);
+    }
+    equal(tree.find(-1).isLeaf(), true, 'left leaf');
+    equal(tree.find(5).isLeaf(), true, 'right leaf');
+    _results = [];
+    for (n = _j = 0; _j <= 4; n = ++_j) {
+      _results.push(equal(tree.find(n).isLeaf(), false));
+    }
+    return _results;
+  });
+
+  test('rotate', function() {
+    var i, index, node, tree, v, values, _i, _j, _k, _len, _len1, _results;
+    values = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+    tree = new DJC.BinarySearchTree(0);
+    for (_i = 0, _len = values.length; _i < _len; _i++) {
+      i = values[_i];
+      tree.insert(i);
+    }
+    for (i = _j = 1; _j <= 100; i = ++_j) {
+      index = Math.floor(Math.random() * values.length);
+      node = tree.find(values[index]);
+      node.rotate();
+    }
+    _results = [];
+    for (_k = 0, _len1 = values.length; _k < _len1; _k++) {
+      v = values[_k];
+      _results.push(equal(tree.find(v) !== null, true));
+    }
+    return _results;
+  });
+
+  test('visitPreorder', function() {
+    var i, order, tree, values, _i, _len;
+    values = [-1, 0, 1];
+    order = [0, -1, 1];
+    tree = new DJC.BinarySearchTree(0);
+    for (_i = 0, _len = values.length; _i < _len; _i++) {
+      i = values[_i];
+      tree.insert(i);
+    }
+    return tree.visitPreorder(function(node) {
+      return equal(node.key, order.shift());
+    });
+  });
+
+  test('visitInorder', function() {
+    var i, order, tree, values, _i, _len;
+    values = [-1, 0, 1];
+    order = [-1, 0, 1];
+    tree = new DJC.BinarySearchTree(0);
+    for (_i = 0, _len = values.length; _i < _len; _i++) {
+      i = values[_i];
+      tree.insert(i);
+    }
+    return tree.visitInorder(function(node) {
+      return equal(node.key, order.shift());
+    });
+  });
+
+  test('visitPostorder', function() {
+    var i, order, tree, values, _i, _len;
+    values = [-1, 0, 1];
+    order = [-1, 1, 0];
+    tree = new DJC.BinarySearchTree(0);
+    for (_i = 0, _len = values.length; _i < _len; _i++) {
+      i = values[_i];
+      tree.insert(i);
+    }
+    return tree.visitPostorder(function(node) {
+      return equal(node.key, order.shift());
+    });
+  });
+
+  test('getRoot', function() {
+    var i, n, tree, values, _i, _j, _len, _len1, _results;
+    values = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+    tree = new DJC.BinarySearchTree(0);
+    for (_i = 0, _len = values.length; _i < _len; _i++) {
+      i = values[_i];
+      tree.insert(i);
+    }
+    _results = [];
+    for (_j = 0, _len1 = values.length; _j < _len1; _j++) {
+      n = values[_j];
+      _results.push(equal(tree.find(n).getRoot() === tree, true));
+    }
+    return _results;
+  });
+
+  test('setLeft', function() {
+    var one, two;
+    one = new DJC.BinaryTreeNode;
+    two = new DJC.BinaryTreeNode;
+    one.setLeft(two);
+    equal(one.left === two, true, 'left child is expected');
+    return equal(two.parent === one, true, 'parent is properly assigned');
+  });
+
+  test('setRight', function() {
+    var one, two;
+    one = new DJC.BinaryTreeNode;
+    two = new DJC.BinaryTreeNode;
+    one.setRight(two);
+    equal(one.right === two, true, 'left child is expected');
+    return equal(two.parent === one, true, 'parent is properly assigned');
+  });
+
+  test('getSide', function() {
+    var i, node, tree, values, _i, _len;
+    values = [-1, -2, -3, -4, 1, 2, 3, 4];
+    tree = new DJC.BinarySearchTree(0);
+    for (_i = 0, _len = values.length; _i < _len; _i++) {
+      i = values[_i];
+      tree.insert(i);
+    }
+    node = tree.find(-4);
+    equal(node.parent.getSide(node), 'left', 'found child on expected side of parent');
+    node = tree.find(4);
+    equal(node.parent.getSide(node), 'right', 'found child on expected side of parent');
+    return raises(function() {
+      return node.parent.getSide(tree);
+    });
+  });
+
+  test('setSide', function() {
+    var one, tree, two;
+    tree = new DJC.BinaryTreeNode;
+    one = new DJC.BinaryTreeNode;
+    two = new DJC.BinaryTreeNode;
+    tree.setSide(one, 'left');
+    equal(tree.left === one, true, 'child assigned to left properly');
+    tree.setSide(two, 'right');
+    equal(tree.right === two, true, 'child assigned to right properly');
+    return raises(function() {
+      return tree.setSide(node, 'rihgt');
+    });
+  });
+
+  test('getChildren', function() {
+    var i, neg, one, tree, two, values, _i, _len;
+    values = [-2, -1, -3, 0, 1, 2];
+    tree = new DJC.BinarySearchTree(0);
+    for (_i = 0, _len = values.length; _i < _len; _i++) {
+      i = values[_i];
+      tree.insert(i);
+    }
+    neg = tree.find(-2).getChildren();
+    equal(neg.length, 2, 'expect two children of -1');
+    equal(neg[0].key, -3, 'expect left child of -1 to first');
+    equal(neg[1].key, -1, 'expect right child of -1 to second');
+    one = tree.find(1).getChildren();
+    equal(one.length, 1, 'expect one child of 1');
+    equal(one[0].key, 2, 'expect child of 1 to be 2');
+    two = tree.find(2).getChildren();
+    return equal(two.length, 0, 'expect no children for 2');
+  });
+
+  test('getSibling', function() {
+    var tree;
+    tree = new DJC.BinaryTreeNode(new DJC.BinaryTreeNode, new DJC.BinaryTreeNode);
+    equal(tree.left.getSibling() === tree.right, true, 'left sibling is right');
+    equal(tree.right.getSibling() === tree.left, true, 'right sibling is left');
+    return equal(tree.getSibling(), void 0, 'root has no sibling');
+  });
+
+  module('BinarySearchTree');
+
+  test('insert', function() {
+    var count, i, tree, _i;
+    tree = new DJC.BinarySearchTree(0);
+    for (i = _i = -25; _i <= 25; i = ++_i) {
+      tree.insert(i);
+    }
+    count = 0;
+    tree.visitInorder(function(node) {
+      return count++;
+    });
+    return equal(count, 51, 'expect 51 nodes in search tree after 51 value inserts');
+  });
+
+  test('find', function() {
+    var i, tree, _i, _len, _ref;
+    tree = new DJC.BinarySearchTree(0);
+    _ref = [-25, 1337, 2];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      i = _ref[_i];
+      tree.insert(i);
+    }
+    equal(tree.find(-25) !== null, true);
+    equal(tree.find(1337) !== null, true);
+    equal(tree.find(2) !== null, true);
+    equal(tree.find(null) === null, true);
+    equal(tree.find(-33) === null, true);
+    return equal(tree.find(25) === null, true);
+  });
+
+  module('TreeLayout');
+
+  test('Reingold-Tilford', function() {
+    var result, tree, val, _i;
+    tree = new DJC.BinarySearchTree(0);
+    for (val = _i = -100; _i <= 100; val = ++_i) {
+      tree.insert(val);
+    }
+    result = new DJC.BinaryTreeTidier().layout(tree);
+    equal(result !== null, true, 'expect a result from tidier layout pass');
+    return this;
+  });
+
+  test('Aesthetic 1 - all nodes at a given depth share the same y coordinate', function() {
+    var k, nodeGroups, tidier, tree, v, val, yCoords, _i;
+    tree = new DJC.BinarySearchTree(0);
+    for (val = _i = -5; _i <= 5; val = ++_i) {
+      tree.insert(val);
+    }
+    tidier = new DJC.BinaryTreeTidier;
+    tidier.layout(tree);
+    nodeGroups = {};
+    tree.visitPreorder(function(node, depth) {
+      if (!nodeGroups[depth]) {
+        nodeGroups[depth] = [];
+      }
+      return nodeGroups[depth].push(node);
+    });
+    for (k in nodeGroups) {
+      v = nodeGroups[k];
+      yCoords = _.map(v, function(node) {
+        return node.y;
+      });
+      equal(_.uniq(yCoords).length, 1, 'nodes in depth group share y-coordinate');
+    }
+    return this;
+  });
+
+  test('Aesthetic 2 - left and right children should be to the left and right of the parent in space', function() {
+    var nodes, tidier, tree, val, _i, _len, _ref;
+    tree = new DJC.BinarySearchTree(0);
+    _ref = [-2, -1, -3, 2, 3, 1];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      val = _ref[_i];
+      tree.insert(val);
+    }
+    tidier = new DJC.BinaryTreeTidier;
+    tidier.layout(tree);
+    nodes = [];
+    tree.visitPostorder(function(node) {
+      var parent, side;
+      parent = node.parent;
+      if (!parent) {
+        return;
+      }
+      side = node.parent.getSide(node);
+      if (side === 'left') {
+        return equal(node.x < node.parent.x, true, "Left child (" + node.x + ") positioned to the left of its parent (" + node.parent.x + ")");
+      } else {
+        return equal(node.x > node.parent.x, true, "Right child (" + node.x + ") positioned to the right of its parent (" + node.parent.x + ")");
+      }
+    });
+    return this;
+  });
+
+  test('Aesthetic 3 - children should positioned equidistant from parent', function() {
+    var nodes, tidier, tree, val, _i, _len, _ref;
+    tidier = new DJC.BinaryTreeTidier;
+    tree = new DJC.BinarySearchTree(0);
+    _ref = [-2, -1, -3, 2, 3, 1];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      val = _ref[_i];
+      tree.insert(val);
+    }
+    tidier.layout(tree);
+    nodes = [];
+    tree.visitPostorder(function(node) {
+      var children, expected, left, right;
+      children = node.getChildren();
+      if (children.length !== 2) {
+        return;
+      }
+      left = children[0].x;
+      right = children[1].x;
+      expected = left + Math.abs(left - right) / 2;
+      return equal(expected, node.x, "parent node position (" + node.x + ") matches expected");
+    });
+    return this;
+  });
+
+  test('Aesthetic 4.1 - mirrored subtrees should be reflections of each other visually', function() {
+    var i, left, leftDist, nodes, right, rightDist, tidier, tree, val, _i, _j, _k, _len, _len1, _ref, _ref1, _results;
+    tree = new DJC.BinarySearchTree(0);
+    _ref = [-2, -1, -4, -3, -5];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      val = _ref[_i];
+      tree.insert(val);
+    }
+    _ref1 = [2, 1, 4, 3, 5];
+    for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+      val = _ref1[_j];
+      tree.insert(val);
+    }
+    tidier = new DJC.BinaryTreeTidier;
+    tidier.layout(tree);
+    nodes = [];
+    _results = [];
+    for (i = _k = 1; _k < 5; i = ++_k) {
+      left = tree.find(-i);
+      right = tree.find(i);
+      leftDist = Math.abs(left.x - tree.x);
+      rightDist = Math.abs(right.x - tree.x);
+      _results.push(equal(leftDist, rightDist, 'Reflected nodes should be equidistant from root'));
+    }
+    return _results;
+  });
+
+  test('Aesthetic 4.2 - identical subtrees should be rendered the identically, regardless of position', function() {
+    var lDistance, nodes, rDistance, result, tidier, tree, val, _i, _len, _ref;
+    tree = new DJC.BinarySearchTree(0);
+    _ref = [7, 4, 3, 5, 13, 12, 14, -3, -6, -2, -7, -13, -14, -12];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      val = _ref[_i];
+      tree.insert(val);
+    }
+    tidier = new DJC.BinaryTreeTidier;
+    result = tidier.layout(tree);
+    nodes = [];
+    lDistance = rDistance = void 0;
+    tree.visitPreorder(function(node) {
+      var leftDistance, less, more, rightDistance, three;
+      if (node.key !== 13 && node.key !== -13) {
+        return;
+      }
+      equal(node.left.key, node.key - 1, 'expect lesser value to the left');
+      equal(node.right.key, node.key + 1, 'expect greater value to the right');
+      three = node.x;
+      less = node.left.x;
+      more = node.right.x;
+      leftDistance = less - three;
+      rightDistance = more - three;
+      if (lDistance && rDistance) {
+        equal(lDistance, leftDistance, 'left child distances match');
+        equal(rDistance, rightDistance, 'right child distances match');
+      }
+      lDistance = leftDistance;
+      return rDistance = rightDistance;
+    });
+    return this;
+  });
+
+}).call(this);
